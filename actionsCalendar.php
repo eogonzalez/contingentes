87e1f42ae9713a46c6c7abb3073fbe2618f1fa90
Eder <djgonzalezeder@gmail.com>
2016-08-05 17:12:14 -0600

    * Creacion de funcionalidades para envio de correos * Inicio de creacion de grupos y contactos

--- actionsCalendar.php
+++ actionsCalendar.php
@@ -43,9 +43,8 @@
 						$response['datos'] = $datosRegFuncion;
 						
 					}else{
-						$response['mensaje'] = 'No se puedo realizar consulta SelectTipoEvento';
+						$response['mensaje'] = 'No es posible realizar consulta SelectTipoEvento';
 					}
-
 				}
 
 				if ($_GET['action'] == 'DeleteTipoEvento') {
@@ -63,10 +62,10 @@
 				if ($_GET['action'] == 'InsertEventoCalendario') {
 
 				  	$nombre = $_GET['nombre'];
-				  	$fechainicio =  $_GET['fechainicio'];
+				  	$fechainicia =  $_GET['fechainicia'];
+				  	$color = $_GET['color'];
 
-
-				 	if ($datosRegFuncion = InsertEventoCalendario($mysqli2, $nombre, $fechainicio)) {
+				 	if ($datosRegFuncion = InsertEventoCalendario($mysqli2, $nombre, $fechainicia, $color)) {
 				 		$response['result'] = true;
 				 		$response['mensaje'] = 'Se agrega evento al calendario.';
 				 		$response['datos']= $datosRegFuncion;
@@ -74,8 +73,45 @@
 				 		$response['datos'] = $datosRegFuncion;
 				 		$response['mensaje'] = 'No se pudo agregar evento al calendario.';
 				 	}
-
 				} 
+
+				if ($_GET['action'] == 'SelectEventosCalendario') {
+					if ($datosRegFuncion = SelectEventosCalendario($mysqli2)) {
+						$response['result'] = true;
+						$response['mensaje'] = 'datos devueltos';
+						$response['datos'] = $datosRegFuncion;
+					}else{
+						$response['mensaje'] = 'No es posible realizar consulta de Eventos del Calendario';
+					}
+				}
+
+				if ($_GET['action'] == 'UpdateEventoCalendario') {
+
+					$idevento = (int) $_GET['idevento'];
+					$todoeldia = $_GET['todoeldia'];
+					$fechainicia = $_GET['fechainicia'];
+					$fechafinaliza = $_GET['fechafinaliza'];
+
+					if ($datosRegFuncion = UpdateEventoCalendario($mysqli2, $idevento, $todoeldia, $fechainicia, $fechafinaliza)) {
+						$response['result'] = true;
+						$response['mensaje'] = 'datos devueltos';
+						$response['datos'] = $datosRegFuncion;
+					}else{
+						$response['mensaje'] = 'No es posible realizar actualizacion de Evento.';
+					}
+				}
+
+				if ($_GET['action'] == 'SelectEvento') {
+					$idevento = (int) $_GET['idevento'];
+
+					if ($datosRegFuncion = SelectEvento($mysqli2, $idevento)) {
+						$response['result'] = true;
+						$response['mensaje'] = 'datos devueltos';
+						$response['datos'] = $datosRegFuncion;
+					}else{
+						$response['mensaje'] = 'No es posible realizar consulta del Evento.';
+					}
+				}
 
 			}else{
 				$response['mensaje'] = "Variable Action no Declarada";
@@ -86,7 +122,75 @@
 
 		//salida JSON
 		echo json_encode($response);
-	}else{
+	}elseif (isset($_POST) && !empty($_POST)) {
+
+				// Array de respuesta y regreso en JSON
+		$response = array(
+			'result' 	=> false,
+			'mensaje' 	=> "No fue posible ejecutar la peticion para el calendario",
+			'datos'		=> ""
+		);
+
+		//Incluimos la conexcion a la base de datos
+		include('connection.php');
+		include('calendar.php');
+
+		//conexion con la base de datos
+		if ($errorDbConexion) {
+
+			if (isset($_POST['action'])) {
+
+				if ($_POST['action'] == 'UpdateEventoFormulario') {
+					$idevento = (int) $_POST['idevento'];
+					$nombre = $_POST['nombre'];
+					$todoeldia = $_POST['todoeldia'];
+					$fecha_inicia = $_POST['fecha_inicia'];
+					$fecha_finaliza = $_POST['fecha_finaliza'];
+					$color = $_POST['color'];
+					$lugar = $_POST['lugar'];
+					$descripcion = $_POST['descripcion'];
+					$adjunto = $_POST['adjunto'];
+					$idgrupo = (int) $_POST['idgrupo'];
+					$otroscorreos = $_POST['otroscorreos'];
+					$estado = $_POST['estado'];
+
+					if ($datosRegFuncion = UpdateEventoFormulario($mysqli2, $idevento, $nombre, $todoeldia, $fecha_inicia, 
+						$fecha_finaliza, $color, $lugar, $descripcion, $adjunto, $idgrupo, $otroscorreos, $estado)) {
+						$response['result'] = true;
+						$response['mensaje'] = 'Datos Devueltos';
+						$response['datos'] = $datosRegFuncion;
+					}else{
+						$response['mensaje'] = 'No es posible realizar actualizacion del evento.';
+					}
+				}
+
+				if ($_POST['action'] == 'SendEmail') {
+					$para = $_POST['para'];
+					$nombre = $_POST['nombre'];
+					$asunto = $_POST['asunto'];
+					$descripcion = $_POST['descripcion'];
+
+					if ($datosRegFuncion = SendEmail($para, $nombre, $asunto, $descripcion)) {
+						$response['result'] = true;
+						$response['mensaje'] = 'Correo Enviado';
+						$response['datos'] = $datosRegFuncion;
+					}else{
+						$response['mensaje'] = 'No es posible enviar correo.';
+					}
+				}
+				
+			}else{
+				$response['mensaje'] = "Variable Action no Declarada";
+			}
+			
+		}else{
+			$response['mensaje'] = "Error con la conexion a la base de datos";	
+		}
+
+		//salida JSON
+		echo json_encode($response);
+	}
+	else{
 		echo "No se puede ejecutar este script";
 	}
 
--- calendar.php
+++ calendar.php
@@ -1,5 +1,7 @@
 <?php
+	date_default_timezone_set('Etc/UTC');
 
+	require '/PHPMailer-master/PHPMailerAutoload.php';
 /*
 	Funcion que selecciona los tipos de eventos creados en el calendario
 */
@@ -87,7 +89,7 @@ function DeleteTipoEvento($dbLink, $idtipo_evento){
 	Funcion que Inserta el evento en el calendario al memento de arrastrarlo al calendario
 */
 
-function InsertEventoCalendario($dbLink, $nombre, $fecha_inicia){
+function InsertEventoCalendario($dbLink, $nombre, $fecha_inicia, $color){
 
 
 	/*$newDate = date("Y-m-d H:i:s", strtotime($fecha_inicia));*/
@@ -108,11 +110,11 @@ function InsertEventoCalendario($dbLink, $nombre, $fecha_inicia){
 	*/
 
 	$sql = sprintf("INSERT INTO EVENTOS
-					(nombre, fecha_inicia, fecha_finaliza, todoeldia, estado, 
+					(nombre, color, fecha_inicia, fecha_finaliza, todoeldia, estado, 
 					fecha_creacion, fecha_actualizacion)
 					VALUES
-					('%s', '%s', '%s', 
-					'%s', '%s', '%s', '%s')", $nombre, $fecha_inicia, $fecha_inicia, 'S', 'P', $fecha_actual, $fecha_actual);
+					('%s', '%s', '%s', '%s', 
+					'%s', '%s', '%s', '%s')", $nombre, $color, $fecha_inicia, $fecha_inicia, 'S', 'P', $fecha_actual, $fecha_actual);
 
 	$result = $dbLink->query($sql);
 
@@ -127,5 +129,220 @@ function InsertEventoCalendario($dbLink, $nombre, $fecha_inicia){
 
 	return $response;
 }
+
+
+/*
+	Funcion que obtiene los eventos para pintarlos en el calendario
+*/
+
+function SelectEventosCalendario($dbLink){
+	$response = array();
+
+	$sql = 'SELECT idevento, nombre, color, fecha_inicia, fecha_finaliza, todoeldia
+			FROM EVENTOS' ;
+
+	$result = $dbLink->query($sql);
+
+	if ($result->num_rows != 0) {
+
+		while ($registro = $result->fetch_array()) {
+			$response [] = $registro;
+		}
+	}
+
+	return $response;
+}
+
+/*
+	Funcion que actualiza el evento al momento de moverlo o cambiar la hora dentro del calendario
+*/
+
+function UpdateEventoCalendario($dbLink, $idevento, $todoeldia, $fecha_inicia, $fecha_finaliza){
+	$response = array();
+	$dia = (string) date("d");
+	$mes = (string) date("m");
+	$anio = (string) date("Y");
+
+	$fecha_actual = $anio."-".$mes."-".$dia;
+
+
+	$sql = sprintf("UPDATE 
+						eventos
+					SET
+						todoeldia = '%s',
+						fecha_inicia = '%s',
+						fecha_finaliza = '%s',
+						fecha_actualizacion = '%s'
+					WHERE idevento = %d ", $todoeldia, $fecha_inicia, $fecha_finaliza, $fecha_actual, $idevento);
+
+	$result = $dbLink->query($sql);
+
+	if ($result == true) {
+		$response[] = true;
+	}else{
+		$response[] = $sql;
+		$response[] = $dbLink->error;
+	}
+
+	$dbLink->close();
+
+	return $response;
+}
+
+
+/*
+	Funcion que selecciona los datos del evento para llenar el formulario de edicion
+*/
+
+function SelectEvento($dbLink, $idevento){
+	$response = array();
+
+	$sql = sprintf('SELECT nombre, color, fecha_inicia, fecha_finaliza, todoeldia, lugar,
+					descripcion, adjunto, idgrupo, otroscorreos, estado
+					FROM EVENTOS
+					where 
+					idevento = %d', $idevento);
+
+	$result = $dbLink->query($sql);
+
+	if ($result->num_rows != 0) {
+		while ($registro = $result->fetch_array()) {
+			$response [] = $registro;
+		}
+	}
+
+	return $response;
+}
+
+/*
+	Funcion que actualiza la informacion del evento del formulario
+*/
+
+function UpdateEventoFormulario($dbLink, $idevento, $nombre, $todoeldia, $fecha_inicia, $fecha_finaliza, $color, $lugar, $descripcion, $adjunto, $idgrupo, $otroscorreos, $estado){
+
+	$response = array();
+	$dia = (string) date("d");
+	$mes = (string) date("m");
+	$anio = (string) date("Y");
+
+	$fecha_actual = $anio."-".$mes."-".$dia;
+
+	$sql = sprintf("UPDATE 
+						eventos
+					SET
+						nombre = '%s',
+						todoeldia = '%s',
+						fecha_inicia = '%s',
+						fecha_finaliza = '%s',
+						color = '%s',
+						lugar = '%s',
+						descripcion = '%s',
+						adjunto = '%s',
+						idgrupo = '%s',
+						otroscorreos = '%s',
+						estado = '%s',
+						fecha_actualizacion = '%s'
+					WHERE idevento = %d ", $nombre, $todoeldia, $fecha_inicia, $fecha_finaliza, $color, $lugar, $descripcion, $adjunto, $idgrupo, $otroscorreos, $estado, $fecha_actual, $idevento);
+
+	$result = $dbLink->query($sql);
+
+	if ($result == true) {
+		$response[] = true;
+	}else{
+		$response[] = $sql;
+		$response[] = $dbLink->error;
+	}
+
+	$dbLink->close();
+
+	return $response;
+
+}
+
+function SendEmail($para, $nombre, $asunto, $descripcion){
+
+	//Creacion de instancia de PHPMailer
+	$mail = new PHPMailer;
+
+	//Utilizar SMTP
+	$mail->isSMTP();
+
+	//Habilitar SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+
+	$mail->SMTPDebug = 2;
+
+	$mail->Debugoutput = 'html';
+	$mail->Host = 'smtp.gmail.com';
+
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = 587;
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "alertas.dace@gmail.com";
+	
+	//Password to use for SMTP authentication
+	$mail->Password = "DACE87654321";
+
+	//Set who the message is to be sent from
+	$mail->setFrom('alertas.dace@gmail.com', 'Alertas DACE');
+
+	//Set an alternative reply-to address
+	//$mail->addReplyTo('replyto@example.com', 'First Last');
+
+	//Set who the message is to be sent to
+	$mail->addAddress($para, $nombre);
+	//Set the subject line
+	$mail->Subject = $asunto;
+
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	$mail->msgHTML($descripcion);
+
+	//Replace the plain text body with one created manually
+	$mail->AltBody = 'This is a plain-text message body';
+
+	//Attach an image file
+	//$mail->addAttachment('images/phpmailer_mini.png');
+
+	//send the message, check for errors
+	if (!$mail->send()) {
+	    echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	    echo "Message sent!";
+	}
+}
+
+
+
+/*
+select
+a.*, b.nombre, b.email
+from
+(
+select
+e.empresaid, p.productoid
+from 
+empresacontingentes e
+join
+contingentes c on
+c.contingenteid = e.contingenteid
+join
+productos p on
+c.productoid = p.productoid
+where
+c.tratadoid = 1
+and c.productoid = 1) a
+join
+authusuarios b on
+a.empresaid = b.empresaid
+*/
 
 ?>
